4da218208200d5e00e9ba4d05263f7e396667ae8
Nils Daumann <nils_daumann@slindev.com>
2022-01-29 15:54:09 +0100

    Moved files that are part of the page into a "public" directory to not expose the build script or anything else in the root dir.

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@
 if [ -z ${GRAB_SERVER_URL+x} ]; then GRAB_SERVER_URL="https://grab-api-dev.slindev.workers.dev/grab/v1/"; fi
 if [ -z ${GRAB_PAGE_URL+x} ]; then GRAB_PAGE_URL="https://develop.grab-website.pages.dev"; fi
 
-echo "const SERVER_URL = '${GRAB_SERVER_URL}'" > configuration.js
-echo "const PAGE_URL = '${GRAB_PAGE_URL}'" >> configuration.js
+echo "const SERVER_URL = '${GRAB_SERVER_URL}'" > public/configuration.js
+echo "const PAGE_URL = '${GRAB_PAGE_URL}'" >> public/configuration.js
---
+++ public/configuration.js
@@ -0,0 +1,2 @@
+const SERVER_URL = 'https://grab-api-dev.slindev.workers.dev/grab/v1/'
+const PAGE_URL = 'https://develop.grab-website.pages.dev'
