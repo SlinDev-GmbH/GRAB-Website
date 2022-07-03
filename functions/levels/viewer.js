27275d69b2a6683ec0c7124edf4fa82242249435
Nils Daumann <nils_daumann@slindev.com>
2022-07-04 01:59:57 +0200

    Just more testing.

--- functions/levels/viewer.js
+++ functions/levels/viewer.js
@@ -1,6 +1,6 @@
 export async function onRequest(context)
 {
-	const { method, url } = request
+	const { method, url } = context.request
 	const finalURL = new URL(url)
 
 	let levelID = finalURL.searchParams.get("level")
@@ -10,13 +10,22 @@ export async function onRequest(context)
 		if(levelIDComponents.length >= 2)
 		{
 			const lookupString = 'level_info:' + levelIDComponents[0] + ':' + levelIDComponents[1]
-			const levelInfo = await env.LEVEL_DB.get(lookupString)
+			console.log(lookupString)
+			console.log(context.env.LEVEL_DB)
+			//const levelList = await context.env.LEVEL_DB.list()
+			//console.log(levelList)
+
+			const levelInfo = await context.env.LEVEL_DB.get(lookupString)
 			if(levelInfo)
 			{
 				return new Response(levelInfo, {
 					status: 200
 				})
 			}
+
+			return new Response(lookupString, {
+					status: 404
+				})
 		}
 	}
 
