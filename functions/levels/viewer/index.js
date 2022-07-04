85963d6129563c2374a30cb22e50551d0dbd0d54
Nils Daumann <nils_daumann@slindev.com>
2022-07-04 12:38:40 +0200

    Fetching from KV instead of hardcoded level info…

--- functions/levels/viewer/index.js
+++ functions/levels/viewer/index.js
@@ -10,8 +10,8 @@ export async function onRequest(context)
 		if(levelIDComponents.length >= 2)
 		{
 			const lookupString = 'level_info:' + levelIDComponents[0] + ':' + levelIDComponents[1]
-			//const levelInfo = await context.env.LEVEL_DB.get(lookupString, { type: "json" })
-			const levelInfo = JSON.parse('{"identifier":"291d931uxfl4ub4dfmvjs:1637879683","title":"blubb","complexity":8,"format_version":3,"iteration":3,"creation_timestamp":1640003463900,"update_timestamp":1640004818729,"data_key":"level_data:291d931uxfl4ub4dfmvjs:1637879683:3","levellist_ok_timestamp":1640003463900,"hidden":false,"levellist_search_title":"blubb_1640003463900","levellist_hidden_timestamp":1643117337869,"tags":["ok"]}')
+			const levelInfo = await context.env.LEVEL_DB.get(lookupString, { type: "json" })
+			//const levelInfo = JSON.parse('{"identifier":"291d931uxfl4ub4dfmvjs:1637879683","title":"blubb","complexity":8,"format_version":3,"iteration":3,"creation_timestamp":1640003463900,"update_timestamp":1640004818729,"data_key":"level_data:291d931uxfl4ub4dfmvjs:1637879683:3","levellist_ok_timestamp":1640003463900,"hidden":false,"levellist_search_title":"blubb_1640003463900","levellist_hidden_timestamp":1643117337869,"tags":["ok"]}')
 
 			if(levelInfo)
 			{
