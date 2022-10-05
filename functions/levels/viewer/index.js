1883365292862ac6fb89adfa0086ce63da5ec0fb
Nils Daumann <nils_daumann@slindev.com>
2022-10-05 17:23:19 +0200

    more

--- functions/levels/viewer/index.js
+++ functions/levels/viewer/index.js
@@ -17,6 +17,8 @@ export async function onRequest(context)
 			const lookupString = 'level_info:' + levelIDComponents[0] + ':' + levelIDComponents[1]
 			const levelInfo = await context.env.LEVEL_DB.get(lookupString, { type: "json" })
 			//const { levelInfo, levelMetadata } = await context.env.LEVEL_DB.getWithMetadata(lookupString, { type: "json" });
+
+			return new Response("result: " + levelInfo, { status: 404 })
 			
 			if(levelInfo)
 			{
