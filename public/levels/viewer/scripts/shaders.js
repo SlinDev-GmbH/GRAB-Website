b0042a9f3ba71596c7fa84e377bd022b37253a4d
Nils Daumann <nils_daumann@slindev.com>
2022-12-14 22:44:20 +0100

    Moved shaders into a js module out of the html.

---
+++ public/levels/viewer/scripts/shaders.js
@@ -0,0 +1,177 @@
+export const shaderSkyVS = `
+	varying vec3 vWorldPosition;
+
+	void main()
+	{
+		vec3 rotatedPosition = (modelViewMatrix * vec4(position, 0.0)).xyz;
+		gl_Position = projectionMatrix * vec4(rotatedPosition, 0.0);
+		gl_Position.z = gl_Position.w;
+
+		vWorldPosition = position;
+	}`
+
+export const shaderSkyFS = `
+	varying vec3 vWorldPosition;
+
+	uniform vec3 cameraFogColor0;
+	uniform vec3 cameraFogColor1;
+	uniform float sunSize;
+
+	uniform vec3 sunColor;
+	uniform vec3 sunDirection;
+
+	void main()
+	{
+		vec3 cameraToVertex = normalize(vWorldPosition);
+
+		float horizonFactor = 1.0 - clamp(abs(cameraToVertex.y) / 0.8, 0.0, 1.0);
+		vec3 fogColor = mix(cameraFogColor1.rgb, cameraFogColor0.rgb, horizonFactor * horizonFactor);
+		vec4 color = vec4(fogColor, 1.0);
+
+		float sunAngle = acos(dot(sunDirection, -cameraToVertex));
+		float sunSize = 0.05 * sunSize;
+		float sunGlowSize = sunSize * 40.0;
+		float sunFactor = clamp((sunGlowSize - sunAngle) / sunGlowSize, 0.0, 1.0);
+		sunFactor *= sunFactor;
+		if(sunAngle < sunSize) sunFactor = 1.5;
+		color.rgb = mix(color.rgb, sunColor, sunFactor);
+
+		gl_FragColor = LinearTosRGB(color);
+	}`
+
+export const shaderLevelVS = `
+	varying vec3 vWorldPosition;
+	varying vec3 vNormal;
+
+	uniform mat3 worldNormalMatrix;
+
+	void main()
+	{
+		vec4 worldPosition = modelMatrix * vec4(position, 1.0);
+		vWorldPosition = worldPosition.xyz;
+
+		vNormal = worldNormalMatrix * normal;
+
+		gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+	}`
+
+export const shaderLevelFS = `
+	varying vec3 vWorldPosition;
+	varying vec3 vNormal;
+
+	uniform sampler2D colorTexture;
+	uniform float tileFactor;
+	uniform vec3 diffuseColor;
+	uniform vec3 sunDirection;
+
+	void main()
+	{
+		vec3 lightDirection = normalize(sunDirection);
+
+		float lightFactor = 0.5 + dot(normalize(vNormal), lightDirection) * 0.5 + 0.5;
+
+		vec4 color = vec4(lightFactor, lightFactor, lightFactor, 1.0);
+
+		vec3 blendNormals = abs(vNormal);
+		if(blendNormals.x > blendNormals.y && blendNormals.x > blendNormals.z)
+		{
+			color.rgb *= texture2D(colorTexture, vWorldPosition.zy * tileFactor).rgb;
+		}
+		else if(blendNormals.y > blendNormals.z)
+		{
+			color.rgb *= texture2D(colorTexture, vWorldPosition.xz * tileFactor).rgb;
+		}
+		else
+		{
+			color.rgb *= texture2D(colorTexture, vWorldPosition.xy * tileFactor).rgb;
+		}
+
+		color.rgb *= diffuseColor;
+
+		gl_FragColor = color;
+	}`
+
+export const shaderLevelNeonFS = `
+	varying vec3 vWorldPosition;
+	varying vec3 vNormal;
+
+	uniform sampler2D colorTexture;
+	uniform float tileFactor;
+	uniform vec3 diffuseColor;
+
+	void main()
+	{
+		vec4 color;
+
+		vec3 blendNormals = abs(vNormal);
+		if(blendNormals.x > blendNormals.y && blendNormals.x > blendNormals.z)
+		{
+			color.rgb = texture2D(colorTexture, vWorldPosition.zy * tileFactor).rgb;
+		}
+		else if(blendNormals.y > blendNormals.z)
+		{
+			color.rgb = texture2D(colorTexture, vWorldPosition.xz * tileFactor).rgb;
+		}
+		else
+		{
+			color.rgb = texture2D(colorTexture, vWorldPosition.xy * tileFactor).rgb;
+		}
+
+		color.rgb *= diffuseColor;
+
+		gl_FragColor = color;
+	}`
+
+export const shaderStartFinishVS = `
+	varying vec2 vTexcoord;
+
+	void main()
+	{
+		vTexcoord = uv;
+		gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+	}`
+
+export const shaderStartFinishFS = `
+	varying vec2 vTexcoord;
+
+	uniform vec4 diffuseColor;
+
+	void main()
+	{
+		vec4 color = diffuseColor;
+		float factor = vTexcoord.y;
+		factor *= factor * factor;
+		factor = clamp(factor, 0.0, 1.0);
+		color.a = factor;
+
+		gl_FragColor = color;
+	}`
+
+export const shaderSignVS = `
+	varying vec2 vTexcoord;
+	varying vec3 vNormal;
+
+	void main()
+	{
+		vec4 worldNormal = modelMatrix * vec4(normal, 0.0);
+		vNormal = worldNormal.xyz;
+		vTexcoord = uv;
+		gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+	}`
+
+export const shaderSignFS = `
+	varying vec2 vTexcoord;
+	varying vec3 vNormal;
+	uniform sampler2D colorTexture;
+	uniform vec3 sunDirection;
+
+	void main()
+	{
+		vec3 lightDirection = normalize(sunDirection);
+		lightDirection = normalize(lightDirection);
+		float lightFactor = 0.5 + dot(normalize(vNormal), lightDirection) * 0.5 + 0.5;
+
+		gl_FragColor = texture2D(colorTexture, vTexcoord) * lightFactor;
+	}`
+
+
