93786f620d3a8305b59c801627549266e122d1af
Nils Daumann <nils_daumann@slindev.com>
2023-01-30 00:57:13 +0100

    Simplified to only show the date.

--- functions/levels/viewer/index.js
+++ functions/levels/viewer/index.js
@@ -51,8 +51,8 @@ export async function onRequest(context)
 				if(metaDescription.length > 0) metaDescription += "\n"
 				const creationDate = new Date(levelInfo.creation_timestamp);
 				const updatedDate = new Date(levelInfo.update_timestamp);
-				metaDescription += "Created: " + creationDate.toUTCString() + "\n"
-				if(creationDate != updatedDate) metaDescription += "Last Updated: " + updatedDate.toUTCString() + "\n"
+				metaDescription += "Created: " + creationDate.toDateString() + "\n"
+				if(creationDate != updatedDate) metaDescription += "Last Updated: " + updatedDate.toDateString() + "\n"
 
 				if(statsInfoResponseJson)
 				{
