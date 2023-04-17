fc7cd95b41480dc4d37f81ccd13f0153d7b77644
Nils Daumann <nils_daumann@slindev.com>
2023-04-17 20:51:26 +0200

    No more sending of access token with list requests if not needed.

--- website/src/requests/ListRequest.js
+++ website/src/requests/ListRequest.js
@@ -1,5 +1,6 @@
 export async function listRequest(server, accessToken, listType, searchTerm, maxLevelFormatVersion, userID, nextPage) {
   let requestURL = server + 'list?max_format_version=' + maxLevelFormatVersion
+  let wantsAccessToken = false
   if(listType === 'tab_newest')
   {
     if(searchTerm && searchTerm.length > 0) requestURL += '&type=search&search_term=' + searchTerm
@@ -15,6 +16,7 @@ export async function listRequest(server, accessToken, listType, searchTerm, max
   else if(listType === 'tab_favorite_levels')
   {
     requestURL = server + 'get_favorite_levels'
+    wantsAccessToken = true
   }
   else if(listType === 'tab_search_users')
   {
@@ -23,24 +25,28 @@ export async function listRequest(server, accessToken, listType, searchTerm, max
   else if(listType === 'tab_hidden')
   {
     requestURL += '&type=hidden'
+    //wantsAccessToken = true //Should also require it, but doesn't currently care about it
   }
   else if(listType === 'tab_reported_levels')
   {
     requestURL = server + 'report_list?type=level&max_format_version=' + maxLevelFormatVersion;
+    wantsAccessToken = true
   }
   else if(listType === 'tab_reported_users')
   {
     requestURL = server + 'report_list?type=user';
+    wantsAccessToken = true
   }
   else if(listType === 'tab_banned_users')
   {
     requestURL = server + 'report_list?type=banned_user';
+    wantsAccessToken = true
   }
 
   if(nextPage) requestURL += '&page_timestamp=' + nextPage
 
   let headers = {}
-  if(accessToken) headers['Authorization'] = 'Bearer ' + accessToken
+  if(accessToken && wantsAccessToken) headers['Authorization'] = 'Bearer ' + accessToken
   const response = await fetch(requestURL, { headers: headers })
   if(response.status != 200) {
     return false
