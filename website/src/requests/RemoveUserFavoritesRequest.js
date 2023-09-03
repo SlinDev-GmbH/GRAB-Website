7bc5e56372255b687062e1a0a2cade367d715d23
EB25Ball <91134871+EB25Ball@users.noreply.github.com>
2023-09-04 11:10:21 +1200

    Update RemoveUserFavoritesRequest.js

--- website/src/requests/RemoveUserFavoritesRequest.js
+++ website/src/requests/RemoveUserFavoritesRequest.js
@@ -1,6 +1,6 @@
 export async function removeUserFavorites(server, levelID, accessToken) {
   const response = await fetch(server + 'remove_favorite_level?level_id=' + levelID + '&access_token=' + accessToken)
-  const responseBody = await response.text();
+  const responseBody = await response.text()
   if(response.status != 200 || responseBody !== 'Success') {
     confirm("Error: " + responseBody);
     return false
