8673d655dc98001eda11ec14ed8f688b1f6941c5
twhlynch <twhlynch@gmail.com>
2023-09-18 22:57:20 +1000

    Unhide button + only show hide/unhide when needed

---
+++ website/src/components/UnhideLevelButton.vue
@@ -0,0 +1,41 @@
+<script>
+import { mapState } from 'pinia'
+import { useUserStore } from '@/stores/user'
+
+import { unhideLevelRequest } from '../requests/UnhideLevelRequest'
+
+export default {
+  props: {
+    level_id : String
+  },
+
+  methods: {
+    async doModerationAction() {
+      if(!await unhideLevelRequest(this.$api_server_url, this.accessToken, this.level_id)) return
+    },
+  }
+}
+</script>
+
+<template>
+  <div class="moderation-unhide-level-button-container">
+    <button class="moderation-unhide-level-button" @click="doModerationAction">Unhide</button>
+  </div>
+</template>
+
+<style>
+.moderation-unhide-level-button {
+  min-width:30%;
+  height: 30px;
+  font-weight: bold;
+  background-color: #ffae00;
+  color: white;
+  border: none;
+  border-radius: 15px;
+  cursor: pointer;
+}
+
+.moderation-unhide-level-button-container {
+  margin-top: 5px;
+}
+</style>
