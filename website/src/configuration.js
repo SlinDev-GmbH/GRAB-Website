03fb83cfead7dde659809c6971879d3339f07d0e
Nils Daumann <nils_daumann@slindev.com>
2023-12-30 01:41:47 +0100

    Updated max level version to 8 and added new protobuf file.

--- website/src/configuration.js
+++ website/src/configuration.js
@@ -1,4 +1,4 @@
 export const SERVER_URL = 'https://api.slin.dev/grab/v1/'
 export const IMAGES_SERVER_URL = 'https://grab-images.slin.dev/'
 export const PAGE_URL = 'https://localhost:5173'
-export const MAX_FORMAT_VERSION = 7
+export const MAX_FORMAT_VERSION = 8
