4880c2a71e5f3a5162442ee9a656631b5aca6b35
twhlynch <twhlynch@gmail.com>
2024-01-02 15:50:08 +1100

    Update favorite and report buttons, and added report svg

--- website/levels/viewer/scripts/level_viewer.js
+++ website/levels/viewer/scripts/level_viewer.js
@@ -712,7 +712,7 @@ function init()
 						object.initialRotation = object.quaternion.clone()
 
 						let signText = node.levelNodeSign.text
-						if(userStore.isModerator && signText && signText.length > 0)
+						if(userStore.isVerifier && signText && signText.length > 0)
 						{
 							let signTextElement = document.createElement("div");
 							const signTextNode = document.createTextNode("Sign " + signCounter + ": " + signText);
