5177e760f7d4a0a2a714bfe5ae034c1679058e14
EB25Ball <91134871+EB25Ball@users.noreply.github.com>
2024-02-28 14:50:06 -0500

    lowercased

--- website/src/components/PlayerViewer.vue
+++ website/src/components/PlayerViewer.vue
@@ -1273,7 +1273,7 @@ export default {
 
 <template>
   <main id="body1" class="scopedAlternative">
-    <button class="buttons" id="download" @click="downloadPlayerPic">Download (.PNG)</button>
+    <button class="buttons" id="download" @click="downloadPlayerPic">Download (.png)</button>
     <a class="buttons" id="back" @click="handleGoBack">Back</a>
     <div id="player-container">
       <canvas class="player-model" id="player-model"></canvas>
