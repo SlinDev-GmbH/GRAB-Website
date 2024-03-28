c644d572d87185b88b9e5450e4f04bbd8a2475d2
Tom Lynch <twhlynch@gmail.com>
2024-03-28 20:37:31 +1100

    punish duration, and moderation tools layout changes

--- website/src/requests/ModerationActionRequest.js
+++ website/src/requests/ModerationActionRequest.js
@@ -1,10 +1,13 @@
-export async function moderationActionRequest(server, accessToken, userID, reason) {
+export async function moderationActionRequest(server, accessToken, userID, reason, duration=undefined) {
   let extra = ''
   if(reason === "level_glitch") {
     extra += "?reason=message&type=message&message=A+level+you+published+relies+on+a+glitch+that+is+not+working+anymore.+If+you+fix+the+level,+please+let+me+know+through+discord+or+tiktok+to+make+it+available+again."
   }
   else {
     extra += '?reason=' + reason
+  }
+  if (duration && duration > 0) {
+    extra += '&duration=' + duration
   }
   const response = await fetch(server + 'moderation_action/' + userID + extra, {headers: {'Authorization': 'Bearer ' + accessToken}})
   const responseBody = await response.text();
