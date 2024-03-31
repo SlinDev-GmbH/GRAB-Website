e4b60e157938d1a33c3f21662a1812482a3609dd
Tom Lynch <twhlynch@gmail.com>
2024-03-31 19:20:43 +1100

    bug fix - make creator not including access token

--- website/src/requests/MakeCreatorRequest.js
+++ website/src/requests/MakeCreatorRequest.js
@@ -1,5 +1,5 @@
 export async function makeCreatorRequest(server, userID, accessToken) {
-  const response = await fetch(server + 'set_user_info_admin/' + userID + '&access_token=' + accessToken + '&is_creator=true')
+  const response = await fetch(server + 'set_user_info_admin/' + userID + '&is_creator=true', {headers: {'Authorization': 'Bearer ' + accessToken}})
   const responseBody = await response.text()
   if(response.status != 200 || responseBody !== 'Success') {
     confirm("Error: " + responseBody);
