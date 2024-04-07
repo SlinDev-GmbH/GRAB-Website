bc7ae39a3b8821a9e35c14de6f4d106e647854c6
EB25Ball <91134871+EB25Ball@users.noreply.github.com>
2024-04-07 16:03:42 -0400

    v1 switch back accidentally switched to v2

--- website/src/requests/GetLevelBrowserRequest.js
+++ website/src/requests/GetLevelBrowserRequest.js
@@ -1,5 +1,5 @@
 export async function GetLevelBrowserRequest(server) {
-  const response = await fetch(server + 'get_level_browser?version=2')
+  const response = await fetch(server + 'get_level_browser?version=1')
   const responseBody = await response.text();
   if(response.status != 200) {
     return false
