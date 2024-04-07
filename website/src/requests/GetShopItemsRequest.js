59a87890d5c316f0f216e96a1fd2a2e266e6fb43
EB25Ball <91134871+EB25Ball@users.noreply.github.com>
2024-04-07 16:04:09 -0400

    v2 switch

--- website/src/requests/GetShopItemsRequest.js
+++ website/src/requests/GetShopItemsRequest.js
@@ -1,5 +1,5 @@
 export async function getShopItemsRequest(server) {
-  const response = await fetch(server + 'get_shop_items?version=1')
+  const response = await fetch(server + 'get_shop_items?version=2')
   const responseBody = await response.text();
   if(response.status != 200) {
       return false
