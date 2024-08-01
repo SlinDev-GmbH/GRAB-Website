40662ad3e7db87b594b929b95557690618d709a2
Nils Daumann <nils_daumann@slindev.com>
2024-08-01 19:18:37 +0200

    stuff

--- website/src/requests/GetShopProductsRequest.js
+++ website/src/requests/GetShopProductsRequest.js
@@ -1,5 +1,5 @@
 export async function getShopProductsRequest(server) {
-    const response = await fetch(server + 'get_shop_products?version=2')
+    const response = await fetch(server + 'get_shop_products?version=3')
     const responseBody = await response.text();
     if(response.status != 200) {
         return false
