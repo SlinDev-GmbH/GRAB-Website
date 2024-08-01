5292ed5e242301e39436c890c800be134fc8d9ba
Nils Daumann <nils_daumann@slindev.com>
2024-08-01 19:22:52 +0200

    argh

--- website/src/requests/GiftCosmeticRequest.js
+++ website/src/requests/GiftCosmeticRequest.js
@@ -1,5 +1,5 @@
 export async function giftCosmeticRequest(server, accessToken, userID, cosmeticID) {
-  const response = await fetch(server + 'add_purchase_admin?user_id=' + userID + '&version=2&sku=' + cosmeticID, {headers: {'Authorization': 'Bearer ' + accessToken}})
+  const response = await fetch(server + 'add_purchase_admin?user_id=' + userID + '&version=3&sku=' + cosmeticID, {headers: {'Authorization': 'Bearer ' + accessToken}})
   const responseBody = await response.text();
   if(response.status != 200 || responseBody !== 'Success') {
     confirm("Error: " + responseBody);
