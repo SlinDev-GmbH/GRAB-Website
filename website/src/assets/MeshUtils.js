e8ede35918198bb548150d82b463edc2b2a5471b
EB Spark <91134871+EB25Ball@users.noreply.github.com>
2024-08-27 20:36:16 -0400

    MeshUtils.js Update

--- website/src/assets/MeshUtils.js
+++ website/src/assets/MeshUtils.js
@@ -2,107 +2,109 @@ import * as THREE from 'three';
 
 class MeshUtils {
 
-  // Function to convert a color from linear to gamma space
-  static linearToGamma([r, g, b]) {
-    r = (r <= 0.0031308) ? 12.92 * r : 1.055 * Math.pow(r, 1 / 2.4) - 0.055;
-    g = (g <= 0.0031308) ? 12.92 * g : 1.055 * Math.pow(g, 1 / 2.4) - 0.055;
-    b = (b <= 0.0031308) ? 12.92 * b : 1.055 * Math.pow(b, 1 / 2.4) - 0.055;
-    return [r, g, b];
+  static linearToGamma(color) {
+    return color.map(c => c <= 0.0031308 ? 12.92 * c : 1.055 * Math.pow(c, 1 / 2.4) - 0.055);
   }
 
-  // Static method to reduce color brightness by 50%
   static halfColor(color) {
-    return new THREE.Color(color).multiplyScalar(0.5);
+    return color.multiplyScalar(0.5);
   }
 
-  // Optimized applyColors function
+  static getColorFromInput(colorInput) {
+    return colorInput instanceof THREE.Color
+      ? colorInput
+      : new THREE.Color(...(Array.isArray(colorInput) ? MeshUtils.linearToGamma(colorInput) : [colorInput]));
+  }
+
   static applyColors(scene, item, group) {
-    const primaryColor = new THREE.Color(...MeshUtils.linearToGamma(scene.userData.primary_color));
-    const secondaryColor = new THREE.Color(...MeshUtils.linearToGamma(scene.userData.secondary_color));
+    const primaryColor = MeshUtils.getColorFromInput(scene.userData.primary_color);
+    const secondaryColor = MeshUtils.getColorFromInput(scene.userData.secondary_color);
 
     const colorMap = {
-      "default_color": index => new THREE.Color(...MeshUtils.linearToGamma(item.materials[index].diffuseColor)),
-      "default_primary_color": () => primaryColor,
-      "default_secondary_color": () => secondaryColor,
-      "default_secondary_color_visor": () => MeshUtils.halfColor(secondaryColor.clone()),
-      "default_secondary_color_darkened": () => MeshUtils.halfColor(secondaryColor.clone()),
-      "default_primary_color_darkened": () => MeshUtils.halfColor(primaryColor.clone()),
-      "default_primary_color_visor": () => MeshUtils.halfColor(primaryColor.clone()),
+      default_color: (index) => new THREE.Color(...MeshUtils.linearToGamma(item.materials[index].diffuseColor)),
+      default_primary_color: () => primaryColor,
+      default_secondary_color: () => secondaryColor,
+      default_secondary_color_visor: () => MeshUtils.halfColor(secondaryColor.clone()),
+      default_secondary_color_darkened: () => MeshUtils.halfColor(secondaryColor.clone()),
+      default_primary_color_darkened: () => MeshUtils.halfColor(primaryColor.clone()),
+      default_primary_color_visor: () => MeshUtils.halfColor(primaryColor.clone()),
     };
 
-    group.children.forEach((obj, index) => {
-      const getColor = colorMap[obj.name];
-      if (getColor) {
-        obj.material.color.set(getColor(index));
+    group.traverse((obj) => {
+      if (obj.material && colorMap[obj.name]) {
+        obj.material.color.set(colorMap[obj.name](group.children.indexOf(obj)));
       }
     });
 
     return group;
   }
 
-  // Method to apply material indices to the group
+  static applyColorsToAll(scene) {
+    const primaryColor = MeshUtils.getColorFromInput(scene.userData.primary_color);
+    const secondaryColor = MeshUtils.getColorFromInput(scene.userData.secondary_color);
+
+    const colorMap = {
+      default_primary_color: () => primaryColor,
+      default_secondary_color: () => secondaryColor,
+      default_secondary_color_visor: () => MeshUtils.halfColor(secondaryColor.clone()),
+      default_secondary_color_darkened: () => MeshUtils.halfColor(secondaryColor.clone()),
+      default_primary_color_darkened: () => MeshUtils.halfColor(primaryColor.clone()),
+      default_primary_color_visor: () => MeshUtils.halfColor(primaryColor.clone()),
+    };
+
+    scene.traverse((obj) => {
+      if (obj.material && colorMap[obj.name]) {
+        obj.material.color.set(colorMap[obj.name]());
+      }
+    });
+  }
+
   static applyMaterialIndices(group, item) {
-    if (!item || !item.materials) {
-      for (let child of group.children) {
-        child.name = "default";
-      }
-    } else {
-      for (let index in item.materials) {
-        if (group.children[index] !== undefined) {
-          group.children[index].name = item.materials[index].type ? item.materials[index].type : item.materials[index];
-        }
-      }
-    }
+    group.children.forEach((child, index) => {
+      child.name = item?.materials?.[index]?.type || item?.materials?.[index] || "default";
+    });
     return group;
   }
 
-  // Method to adjust the group's position/rotation based on item type
   static adjustGroupForCategory(group, itemtype) {
     if (itemtype.includes('body')) {
       group.position.copy(new THREE.Vector3(0, -0.2, 0));
 
     } else if (itemtype === 'hand') {
-      let hands = [group, group.clone()];
-      hands[1].position.set(0.3, -0.5, -0.4);//left hand
-      hands[1].rotation.z = 170*(Math.PI/180)
-      hands[1].rotation.x = 10*(Math.PI/180)
-      hands[1].rotation.y = 0
-// 50/50 this is right
+      const handsGroup = new THREE.Group();
+      const leftHand = group.clone()
+      const rightHand = group
 
+      leftHand.position.set(0.3, -0.5, -0.4)
+      leftHand.rotation.set(10 * Math.PI / 180, 0, 170 * Math.PI / 180)
 
-      hands[0].position.set(-0.3, -0.75, -0.1);
-      hands[0].rotation.x = -45*(Math.PI/180)
-      hands[0].rotation.y = 0
-      hands[0].rotation.z = -190*(Math.PI/180)      
-      hands[0].scale.set(-1,1,1) 
+      rightHand.position.set(-0.3, -0.75, -0.1)
+      rightHand.rotation.set(-45 * Math.PI / 180, 0, -190 * Math.PI / 180)
+      rightHand.scale.set(-1, 1, 1)
 
-
-
-
-
-
-
-      let handsGroup = new THREE.Group();
-      handsGroup.add(hands[0]);
-      handsGroup.add(hands[1]);
-
+      handsGroup.add(leftHand, rightHand);
       return handsGroup;
     } else if (itemtype === "checkpoint") {
-      group.position.set(-0.5, -1.5, 0);
+      group.position.set(-0.5, -1.5, 0)
     } else if(itemtype == "rope/left" || itemtype === "grapple/hook/left") {
-      group.position.set(0.3, -0.5, -0.4);
-      // group.rotation.z = 170*(Math.PI/180)
-      // group.rotation.x = 10*(Math.PI/180)
-      // group.rotation.y = 0
+      group.position.set(0.3, -0.5, -0.4)
+      group.rotation.z = 170*(Math.PI/180)
+      group.rotation.x = 10*(Math.PI/180)
+      group.rotation.y = 0
 
     } else if(itemtype == "rope/right" || itemtype === "grapple/hook/right") {
 
       group.position.set(-0.3, -0.75, -0.1);
-      // group.rotation.x = -45*(Math.PI/180)
-      // group.rotation.y = 0
-      // group.rotation.z = -190*(Math.PI/180)      
-      group.scale.set(-1,1,1)  // should just copy the hands tbh
+      group.rotation.x = -45*(Math.PI/180)
+      group.rotation.y = 0
+      group.rotation.z = -190*(Math.PI/180)      
+      group.scale.set(-1,1,1)  
 
+    }
+    if (itemtype.includes("grapple/hook")) {
+      let offset = new THREE.Vector3(0, 0, -1)
+      offset.applyQuaternion(group.quaternion)
+      group.position.add(offset)
     }
     return group;
   }
