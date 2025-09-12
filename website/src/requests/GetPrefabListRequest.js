643a2c9c2b21b4046dfdce273b9b19f90486a1ba
Tom Lynch <twhlynch@gmail.com>
2025-09-12 13:33:10 +1000

    prefab list request

---
+++ website/src/requests/GetPrefabListRequest.js
@@ -0,0 +1,8 @@
+export async function getPrefabListRequest(server, accessToken, userID, maxFormatVersion, cursor) {
+    const response = await fetch(`${server}prefab_list?user_id=${userID}&max_format_version=${maxFormatVersion}${cursor ? `&cursor=${cursor}` : ''}`, {headers: {'Authorization': 'Bearer ' + accessToken}})
+    const responseBody = await response.text();
+    if(response.status != 200) {
+        return false
+    }
+    return JSON.parse(responseBody)
+}
