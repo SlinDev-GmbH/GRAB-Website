2dd06de9bca808dd6d2460a8c8dcdc4fc623477c
Tom Lynch <twhlynch@gmail.com>
2025-09-12 13:33:05 +1000

    prefab block request

---
+++ website/src/requests/PrefabBlockRequest.js
@@ -0,0 +1,13 @@
+export async function prefabBlockRequest(server, accessToken, userID, prefabID) {
+    const response = await fetch(`${server}prefab_block/${userID}/${prefabID}`,
+    {headers: {'Authorization': 'Bearer ' + accessToken}})
+
+    const responseBody = await response.text();
+    if(response.status != 200 || responseBody !== 'Success') {
+        confirm("Error: " + responseBody);
+        return false
+    }
+
+    return true
+
+}
