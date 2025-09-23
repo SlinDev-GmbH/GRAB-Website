df3aced2f47b5724c9deb98d9bcdb5d615a8dc3a
Tom Lynch <twhlynch@gmail.com>
2025-09-23 23:52:53 +1000

    fix missing / in url

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 if [ -z ${GRAB_SERVER_URL+x} ]; then GRAB_SERVER_URL="https://grab-api-dev.slindev.workers.dev/grab/v1/"; fi
 if [ -z ${GRAB_DATA_URL+x} ]; then GRAB_DATA_URL="https://grab-data-dev.slin.dev/"; fi
 if [ -z ${GRAB_IMAGES_SERVER_URL+x} ]; then GRAB_IMAGES_SERVER_URL="https://grab-images-dev.slin.dev/"; fi
-if [ -z ${GRAB_PAGE_URL+x} ]; then GRAB_PAGE_URL="https://develop.grab-website.pages.dev"; fi
+if [ -z ${GRAB_PAGE_URL+x} ]; then GRAB_PAGE_URL="https://develop.grab-website.pages.dev/"; fi
 
 echo "export const SERVER_URL = '${GRAB_SERVER_URL}'" > website/src/configuration.js
 echo "export const DATA_URL = '${GRAB_DATA_URL}'" >> website/src/configuration.js
