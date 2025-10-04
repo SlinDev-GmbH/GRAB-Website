e4cdfbd41e534a77d3eabd38b310ccb9e5cea91e
Tom Lynch <twhlynch@gmail.com>
2025-10-05 00:55:19 +1000

    update dev env format version

--- website/src/configuration.js
+++ website/src/configuration.js
@@ -2,4 +2,4 @@ export const SERVER_URL = 'https://api.slin.dev/grab/v1/'
 export const DATA_URL = 'https://grab-data.slin.dev/'
 export const IMAGES_SERVER_URL = 'https://grab-images.slin.dev/'
 export const PAGE_URL = 'https://localhost:5173'
-export const MAX_FORMAT_VERSION = 15
+export const MAX_FORMAT_VERSION = 16
