ff9d247d494fcc03c1676313998d55ce4e900649
Tom Lynch <twhlynch@gmail.com>
2025-10-10 22:49:55 +1100

    ensure favouriting doesnt include iteration id

--- website/src/requests/RemoveUserFavoritesRequest.js
+++ website/src/requests/RemoveUserFavoritesRequest.js
@@ -1,4 +1,5 @@
 export async function removeUserFavorites(server, levelID, accessToken) {
+	levelID = levelID.split(/[:/]/).slice(0, 2).join(':');
 	const response = await fetch(server + 'remove_favorite_level?level_id=' + levelID + '&access_token=' + accessToken);
 	const responseBody = await response.text();
 	if (response.status != 200 || responseBody !== 'Success') {
--- website/src/requests/SetUserFavoritesRequest.js
+++ website/src/requests/SetUserFavoritesRequest.js
@@ -1,4 +1,5 @@
 export async function setUserFavorites(server, levelID, accessToken) {
+	levelID = levelID.split(/[:/]/).slice(0, 2).join(':');
 	const response = await fetch(server + 'add_favorite_level?level_id=' + levelID + '&access_token=' + accessToken);
 	if (response.status != 200) {
 		const responseBody = await response.text();
