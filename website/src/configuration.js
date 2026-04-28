d9eb23006b3a31f5530fdb84455f8a082a48e1b7
Tom Lynch <twhlynch@gmail.com>
2026-04-29 00:12:48 +1000

    add `LOGGING_URL` to config

--- website/src/configuration.js
+++ website/src/configuration.js
@@ -1,5 +1,6 @@
 export const SERVER_URL = 'https://api.slin.dev/grab/v1/';
 export const DATA_URL = 'https://grab-data.slin.dev/';
 export const IMAGES_SERVER_URL = 'https://grab-images.slin.dev/';
+export const LOGGING_URL = 'https://api.slin.dev/grab-logging/v1/';
 export const PAGE_URL = 'https://localhost:5173';
 export const MAX_FORMAT_VERSION = 21;
