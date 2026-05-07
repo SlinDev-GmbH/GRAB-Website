185bf70f2fca533232bd104288b279498d58e985
Tom Lynch <twhlynch@gmail.com>
2026-05-08 00:59:13 +1000

    bump format version

--- website/src/configuration.js
+++ website/src/configuration.js
@@ -3,4 +3,4 @@ export const DATA_URL = 'https://grab-data.slin.dev/';
 export const IMAGES_SERVER_URL = 'https://grab-images.slin.dev/';
 export const LOGGING_URL = 'https://api.slin.dev/grab-logging/v1/';
 export const PAGE_URL = 'https://localhost:5173';
-export const MAX_FORMAT_VERSION = 21;
+export const MAX_FORMAT_VERSION = 22;
